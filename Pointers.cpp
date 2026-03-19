ef1161fdf2d589dd81baa37791532a5e0c9d2f29
Mai Nam Khánh <mainamkhanh20051023@gmail.com>
2026-03-19 22:20:20 +0700

    Pointers.cpp

---
+++ Pointers.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cstdlib>
+void update(int *a,int *b) {
+    // Complete this function 
+    int originalA = *a;
+    int originalB = *b;
+    
+    *a = originalA + originalB;
+    
+    *b = abs(originalA - originalB);
+      
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
